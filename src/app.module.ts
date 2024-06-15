792a7046b75d17ac7094a727a6964c312bb87923
silasms <silas.medeiros7@gmail.com>
2024-06-15 08:39:32 -0300

    refactor: update import list in app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './routes/users/users.module';
+import { TokenModule } from './services/token/token.module';
+import { PrismaModule } from './services/prisma/prisma.module';
 
 @Module({
-  imports: [],
+  imports: [UsersModule, TokenModule, PrismaModule],
   controllers: [AppController],
   providers: [AppService],
 })
