dcbf4871b857b4a906c1862fc281163ad155501d
silasms <silas.medeiros7@gmail.com>
2024-06-15 09:19:44 -0300

    chore: middleware to authentication

---
+++ src/guards/auth.guard.ts
@@ -0,0 +1,31 @@
+import {
+  CanActivate,
+  ExecutionContext,
+  ForbiddenException,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
+import { TokenService } from 'src/services/token/token.service';
+
+@Injectable()
+export class AuthGuard implements CanActivate {
+  constructor(private readonly tokenService: TokenService) {}
+
+  async canActivate(context: ExecutionContext): Promise<boolean> {
+    try {
+      const request = context.switchToHttp().getRequest();
+      const { authorization }: any = request.headers;
+      if (!authorization || authorization.trim() === '') {
+        throw new UnauthorizedException('Please provide token');
+      }
+      const authToken = authorization.replace(/bearer/gim, '').trim();
+      const resp = await this.tokenService.decodeToken(authToken);
+      request.decodedData = resp;
+      return true;
+    } catch (error) {
+      throw new ForbiddenException(
+        error.message || 'session expired! Please sign In',
+      );
+    }
+  }
+}
