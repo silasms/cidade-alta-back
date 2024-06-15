c4ad1e3c335242014d5827c63b5299c769ef277d
silasms <silas.medeiros7@gmail.com>
2024-06-15 08:21:05 -0300

    fix: change imports

--- src/services/token/token.module.ts
+++ src/services/token/token.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { TokenService } from './token.service';
 import { ConfigModule } from '@nestjs/config';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
   providers: [TokenService],
-  imports: [ConfigModule.forRoot()],
+  imports: [ConfigModule.forRoot(), JwtModule],
+  exports: [TokenService],
 })
 export class TokenModule {}
