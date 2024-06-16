1ab416d7c49a0afc033dd73409141dfa6dec0e36
silasms <silas.medeiros7@gmail.com>
2024-06-16 16:09:12 -0300

    docs: swagger instance

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
+  const config = new DocumentBuilder()
+    .setTitle('Alta back')
+    .setDescription('API de documentação para o back do cidade alta!')
+    .setVersion('1.0')
+    .addTag('routes')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
   await app.listen(3000);
 }
 bootstrap();
